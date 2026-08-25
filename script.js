5e6033f90ca5ef8098bccd4f2e3aae28b49ed4a9
kamadaexypra <sambaelputoamo@gmail.com>
2026-08-25 18:12:03 +0200

    Add cambiarColor function to change background color

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ alert("¡Hola! Mi página funciona con JavaScript.");
 function cambiarTexto() {
     document.getElementById("titulo").textContent = "¡Has cambiado el texto!";
 }
+function cambiarColor() {
+    document.body.style.backgroundColor = "lightblue";
+}
